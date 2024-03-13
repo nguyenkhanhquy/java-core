c6d259932d9ea1020aa5cb94be4187e972fb6b07
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-03-14 00:07:01 +0700

    add: java-05

---
+++ java-05/src/ViDu.java
@@ -0,0 +1,17 @@
+
+public class ViDu {
+
+	public static void main(String[] args) {
+		String tenSach = "Lập trình Java";
+		int namXuatBan = 2021;
+		double giaTien = 15.5;
+		boolean conSach = true;
+		char maKho = 'M';
+		System.out.println("Tên sách: " + tenSach);
+		System.out.println("Năm: " + namXuatBan);
+		System.out.println("Giá: " + giaTien);
+		System.out.println("Còn sách: " + conSach);
+		System.out.println("Mã kho: " + maKho);
+	}
+	
+}
