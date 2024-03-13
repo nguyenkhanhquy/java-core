9beb54abfa8066bbae7a65a8e5155a4e55b0fb6b
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-03-14 00:24:43 +0700

    add: java-07

---
+++ java-07/src/ViDu.java
@@ -0,0 +1,18 @@
+/**
+ * @author quy
+ */
+public class ViDu {
+	/**
+	 * @param args: tham số đầu vào
+	 */
+	public static void main(String[] args) {
+		// Khai báo biến
+		int x = 10;
+		
+		/*
+		 * Xuất số x ra màn hình
+		 * Sử dụng hàm sysout
+		 */
+		System.out.println(x);
+	}
+}
