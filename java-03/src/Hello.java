cc4085c2120d38864ef2b6bda253d2dc3ffa5e14
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-03-13 23:57:11 +0700

    add: java-03

---
+++ java-03/src/Hello.java
@@ -0,0 +1,9 @@
+
+public class Hello {
+	// main => ctrl + khoảng trắng => enter
+	public static void main(String[] args) {
+		// sysout => ctrl + khoảng trắng => enter
+		System.out.println("Hello World!");
+	}
+
+}
