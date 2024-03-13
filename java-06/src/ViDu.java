16d5eae2918a20ebfe2905b5430d275c43d07afa
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-03-14 00:09:23 +0700

    add: java-06

---
+++ java-06/src/ViDu.java
@@ -0,0 +1,10 @@
+
+public class ViDu {
+
+	public static void main(String[] args) {
+		final int x = 100;
+		System.out.println("x = "+ x);
+		// x = 50; => sẽ bị báo lỗi khi cố tình thay đổi giá trị của hằng số
+	}
+	
+}
