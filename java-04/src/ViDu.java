9281bde75061dde7550f62a67b9d3a37d57ffa46
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-03-14 00:01:47 +0700

    add: java-04

---
+++ java-04/src/ViDu.java
@@ -0,0 +1,15 @@
+
+public class ViDu {
+
+	public static void main(String[] args) {
+		String thongBao = "Xin chào";
+		String hoVaTen = "Nguyễn Khánh Quy";
+		String tenSach;
+		tenSach = "Lập trình Java";
+		
+		System.out.println("Thông báo: " + thongBao);
+		System.out.println("Họ và tên: " + hoVaTen);
+		System.out.println("Tên sách: " + tenSach);
+	}
+	
+}
