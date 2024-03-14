c7ebaa2cef9513ea4c097938284771c53deca82d
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-03-14 08:32:36 +0700

    add: java-12

---
+++ java-12/src/ViDu.java
@@ -0,0 +1,20 @@
+
+public class ViDu {
+
+	public static void main(String[] args) {
+		int a = 5;
+		boolean b = false;
+		System.out.println("-a => giá tri: " + (-a));
+		System.out.println("+a => giá tri: " + (+a));
+		System.out.println("!b => giá tri: " + (!b));
+		System.out.println("----------");
+		System.out.println("a = " + a);
+		System.out.println("++a = " + (++a));
+		System.out.println("a++ = " + (a++));
+		System.out.println("a = " + a);
+		System.out.println("--a = " + (--a));
+		System.out.println("a-- = " + (a--));
+		System.out.println("a = " + a);
+	}
+	
+}
