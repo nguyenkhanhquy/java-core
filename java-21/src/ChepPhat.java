a5354b70722314bcfcb67337d856383a0714df95
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-03-14 15:46:07 +0700

    add: java-21

---
+++ java-21/src/ChepPhat.java
@@ -0,0 +1,15 @@
+
+public class ChepPhat {
+
+	public static void main(String[] args) {
+		// Bị phạt chép phạt 2000 lần "Em hứa sẽ làm bài tập đầy đủ"
+		for (int i = 1; i <= 2000; i++) {
+			System.out.println(i + ". Em hứa làm bài tập đầy đủ.");
+		}
+		
+		for (int i = 2000; i > 0; i--) {
+			System.out.println(i + ". Em hứa làm bài tập đầy đủ.");
+		}
+	}
+	
+}
