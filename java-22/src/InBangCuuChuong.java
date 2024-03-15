7bd421dc27439cfbc63c58c5c093b25b64bc54b9
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-03-16 00:07:57 +0700

    add: java-22

---
+++ java-22/src/InBangCuuChuong.java
@@ -0,0 +1,13 @@
+
+public class InBangCuuChuong {
+	
+	public static void main(String[] args) {
+		for (int i = 2; i <= 9; i++) {
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(i + " * " + j + " = " + (i*j));
+			}
+			System.out.println("----------");
+		}
+	}
+	
+}
