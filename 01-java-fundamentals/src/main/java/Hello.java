8a7b811bd7c8e91b22178ed55d6f2b1beadc06a1
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-10-26 22:20:31 +0700

    add: 01-java-fundamentals

---
+++ 01-java-fundamentals/src/main/java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World of Java!");
+        System.out.println("My favorite color is blue!");
+        System.out.println("My hobby is coding!");
+    }
+}
