43a1019a6bbc95754dbadd73d3253333d568a523
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-10-27 17:47:36 +0700

    add: Method Demo

---
+++ 04-methods/src/main/java/MethodDemo.java
@@ -0,0 +1,12 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+        // Call the method
+        displayGreetings();
+    }
+
+    // Define the method
+    static void displayGreetings() {
+        System.out.println("Hello World!");
+        System.out.println("Welcome, welcome.");
+    }
+}
